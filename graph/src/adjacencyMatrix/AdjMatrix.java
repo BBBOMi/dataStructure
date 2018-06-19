d0ec2691e7454c83e7ae32e6fbbc88c9fc486de6
bomi <a51022866@gmail.com>
2018-06-19 15:58:59 +0900

    modify_range

--- graph/src/adjacencyMatrix/AdjMatrix.java
+++ graph/src/adjacencyMatrix/AdjMatrix.java
@@ -9,7 +9,7 @@ public class AdjMatrix {
 	}
 	
 	public void insertEdge(int v1, int v2) {
-		if(v1 > totalV || v2 > totalV) {
+		if(v1 >= totalV || v2 >= totalV) {
 			System.out.println("그래프에 없는 정점입니다.");
 		} else {
 			matrix[v1][v2] = 1;
